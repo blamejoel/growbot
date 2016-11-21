d272428be24ce090ca87bd23677c65d88c0751cd
Joel Gomez <joel.gomez@email.ucr.edu>
2016-11-21 15:06:23 -0800

    added some defines to make nrf24l01_plus a bit more modular

--- lib/nrf24L01_plus/gateway/radioPinFunctions.c
+++ lib/nrf24L01_plus/gateway/radioPinFunctions.c
@@ -15,25 +15,33 @@
 #define clr_bit(reg,bit) reg &= ~(1<<bit)
 #define check_bit(reg,bit) (reg&(1<<bit))
 
+#define NRF_DDR   DDRC
+#define NRF_PORT  PORTC
+#define NRF_CE    0       // Chip Enable
+#define NRF_CSN   1       // Chip Select Not  (same function as SS')
+#define NRF_SCK   2       // SPI Clock
+#define NRF_MOSI  3       // SPI Master Output Slave Input
+#define NRF_MISO  4       // SPI Master Input Slave Output
+
 /* ------------------------------------------------------------------------- */
 void nrf24_setupPins()
 {
-    set_bit(DDRC,0); // CE output
-    set_bit(DDRC,1); // CSN output
-    set_bit(DDRC,2); // SCK output
-    set_bit(DDRC,3); // MOSI output
-    clr_bit(DDRC,4); // MISO input
+    set_bit(NRF_DDR,NRF_CE);    // CE output
+    set_bit(NRF_DDR,NRF_CSN);   // CSN output
+    set_bit(NRF_DDR,NRF_SCK);   // SCK output
+    set_bit(NRF_DDR,NRF_MOSI);  // MOSI output
+    clr_bit(NRF_DDR,NRF_MISO);  // MISO input
 }
 /* ------------------------------------------------------------------------- */
 void nrf24_ce_digitalWrite(uint8_t state)
 {
     if(state)
     {
-        set_bit(PORTC,0);
+        set_bit(NRF_PORT,NRF_CE);
     }
     else
     {
-        clr_bit(PORTC,0);
+        clr_bit(NRF_PORT,NRF_CE);
     }
 }
 /* ------------------------------------------------------------------------- */
@@ -41,11 +49,11 @@ void nrf24_csn_digitalWrite(uint8_t state)
 {
     if(state)
     {
-        set_bit(PORTC,1);
+        set_bit(PORTC,NRF_CSN);
     }
     else
     {
-        clr_bit(PORTC,1);
+        clr_bit(PORTC,NRF_CSN);
     }
 }
 /* ------------------------------------------------------------------------- */
@@ -53,11 +61,11 @@ void nrf24_sck_digitalWrite(uint8_t state)
 {
     if(state)
     {
-        set_bit(PORTC,2);
+        set_bit(PORTC,NRF_SCK);
     }
     else
     {
-        clr_bit(PORTC,2);
+        clr_bit(PORTC,NRF_SCK);
     }
 }
 /* ------------------------------------------------------------------------- */
@@ -65,16 +73,16 @@ void nrf24_mosi_digitalWrite(uint8_t state)
 {
     if(state)
     {
-        set_bit(PORTC,3);
+        set_bit(PORTC,NRF_MOSI);
     }
     else
     {
-        clr_bit(PORTC,3);
+        clr_bit(PORTC,NRF_MOSI);
     }
 }
 /* ------------------------------------------------------------------------- */
 uint8_t nrf24_miso_digitalRead()
 {
-    return check_bit(PINC,4);
+    return check_bit(PINC,NRF_MISO);
 }
 /* ------------------------------------------------------------------------- */
